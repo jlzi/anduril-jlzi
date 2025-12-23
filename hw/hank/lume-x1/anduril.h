4e6a342c99de32469ccb1375a731533fbe04e051
jlzi <jeremylindberg@gmail.com>
2025-12-22 22:31:43 -0800

    update

--- hw/hank/lume-x1/anduril.h
+++ hw/hank/lume-x1/anduril.h
@@ -50,7 +50,7 @@
 #define DEFAULT_LEVEL  50
 #define MAX_1x7135     49
 
-#define RAMP_SMOOTH_FLOOR    15
+#define RAMP_SMOOTH_FLOOR    12
 #define RAMP_SMOOTH_CEIL   120
 
 #define RAMP_DISCRETE_FLOOR   10
